eaa7e3e6f9a1c6618aa9ffb0bc033465ad6b0450
jingdingsanmei <275128572@qq.com>
2025-03-14 16:56:06 +0800

    添加视图控制

--- src/pixi/canvas.ts
+++ src/pixi/canvas.ts
@@ -1,4 +1,5 @@
 import * as PIXI from "pixi.js";
+import { Viewport } from 'pixi-viewport';
 import { Scene } from "./scene";
 import { ICanvasContext } from "./types";
 
@@ -6,6 +7,7 @@ export class Canvas {
     public context!: ICanvasContext;
     public domElement: HTMLElement;
     public engine!: PIXI.Application;
+    public viewport!: Viewport;
     public scene!: Scene;
     constructor(domElement: HTMLElement) {
         this.domElement = domElement;
@@ -15,10 +17,21 @@ export class Canvas {
     protected init() {
         this.engine = new PIXI.Application();
         this.context = { engine: this.engine };
-        this.engine.init({ background: 0xeeeeee, resizeTo: window }).then(() => {
+        this.engine.init({ background: 0xeeeeee, width: window.innerWidth, height: window.innerHeight }).then(() => {
             this.domElement.appendChild(this.engine.canvas);
+
+            const viewport = new Viewport({ screenWidth: window.innerWidth, screenHeight: window.innerHeight, worldWidth: 100000, worldHeight: 100000, events: this.engine.renderer.events })
+            this.engine.stage.addChild(viewport);
             this.scene = new Scene(this.context);
-            this.engine.stage.addChild(this.scene.node);
+            viewport.addChild(this.scene.node);
+            // 启用Viewport的拖拽、缩放和鼠标滚轮缩放
+            viewport
+                .drag()
+                .pinch()
+                .wheel()
+                .decelerate();
         });
+
+
     }
 }
